558f92d7795b31f4042547faa2212bd84f21053e
Brant <xkdcc@users.noreply.github.com>
2018-05-31 22:23:38 -0700

    Update sleep.pl

--- sleep.pl
+++ sleep.pl
@@ -6,6 +6,7 @@ use warnings;
 while ( 1 ) {
   if( -e "/log/bc_debug" ) {
     print("BC_DEBUG: Continue debug mode...\n");
+    sleep(10);
   }
   else {
     print("BC_DEBUG: exit debug mode...\n");
